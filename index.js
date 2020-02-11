d0051703f4e47b6703e3883b046dec23198058d9
Борзунов Игорь <Borzunov@lanit>
2020-02-11 16:51:58 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const cloudscraper = require('cloudscraper');
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { window } = new JSDOM();
+const { document } = (new JSDOM('')).window;
+global.document = document;
+
+const $ = jQuery = require('jquery')(window);
+const Telegraf = require('telegraf')
+const fs = require('fs');
+//---------------------------------------
+
+//global vars
+const token = fs.readFileSync('token', 'utf8').split("\n")[0];
+const telegramId = fs.readFileSync('telegramId', 'utf8');
+//slow access
+let newsStore =
+{
+    "https://www.esetnod32.ru/company/press/center/": "",
+}
+
+//bot -----------------
+const bot = new Telegraf(token);
+
+bot.start((ctx) => ctx.reply('Hello, My Friend We are ESET!!!!!!'))
+bot.hears('hi', (ctx) => { ctx.reply(newsStore); })
+bot.launch();
+
+//main logic ----------------------
+function extractNews(news, htmlPage) {
+    const elements = $(htmlPage);
+    const listOfNews = elements.find('tbody').html();
+    if (listOfNews == undefined) {
+        console.log("cant find news");
+        return;
+    }
+    const lastNews = listOfNews.split('\n').filter(NewsStr => NewsStr.length > 5)[0];
+
+    if (lastNews == undefined) {
+        console.log("cant find last news");
+        return;
+    }
+    const cleanText = lastNews.replace("</a>", " || ").replace(/<\/?[^>]+(>|$)/g, "");
+    if (cleanText == undefined) {
+        console.log("cant find cleanText");
+        return;
+    }
+    const oldNews = NewsStore[news];
+    if (oldNews.length > 0 && oldNews != cleanText) {
+        bot.telegram.sendMessage(telegramId, cleanText);
+    }
+    newsStore[news] = cleanText;
+}
+
+
+function poll() {
+    for (let key in NewsStore) {
+        const extractNewsBinded = extractNews.bind(null, key);
+        cloudscraper.get(key).then(extractNewsBinded, console.error);
+    }
+    setTimeout(poll, 15 * 60 * 1000);
+}
+
+poll();
